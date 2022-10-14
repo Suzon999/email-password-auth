cae0998e3b090c4ac2bcb5396a0e794b6571989b
Suzon999 <ashikurrahamansuzon346112.com>
2022-10-14 22:47:03 +0600

    onclick and ona blur handle added

--- src/App.js
+++ src/App.js
@@ -4,8 +4,28 @@ import app from "./firebase/firebase.init";
 
 function App() {
   const auth = getAuth(app);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    console.log(email, password);
+  }
+  const handleEmailBlur = (event) => {
+    console.log(event.target.value)
+  }
+  const handlePasswordBlur = (event) => {
+    console.log(event.target.value)
+  }
+
   return (
     <div className="App">
+      <form onSubmit={handleSubmit}>
+        <input onBlur={handleEmailBlur} type="email" name="email" id="" placeholder=" Your email" />
+        <br />
+        <input onBlur={handlePasswordBlur} type="password" name="password" id="" placeholder="Your Password" />
+        <br />
+        <button type="submit">Regester</button>
+      </form>
 
     </div>
   );
